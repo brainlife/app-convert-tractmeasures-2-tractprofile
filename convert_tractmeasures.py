3a7de231c3ad5399f28e08908277cb1f63bea616
Soichi Hayashi <soichih@gmail.com>
2021-12-10 15:36:44 -0500

    added missing imports

--- convert_tractmeasures.py
+++ convert_tractmeasures.py
@@ -1,28 +1,25 @@
 #!/usr/bin/env python3
 
-def convert_tm(tractmeasures_path,outpath):
+import json
+import pandas as pd
+import os
 
-	tractmeasures = pd.read_csv(tractmeasures_path)
+outpath = 'profiles'
 
-	for i in tractmeasures.structureID.unique():
-		print("converting "+i)
-		tmp = tractmeasures.loc[tractmeasures['structureID'] == i]
-		tmp = tmp.drop(columns={'subjectID','structureID','nodeID'})
-		tmp.to_csv(outpath+'/'+i+'_profiles.csv',index=False)
+def convert_tm(tractmeasures_path):
+    tractmeasures = pd.read_csv(tractmeasures_path)
+    for i in tractmeasures.structureID.unique():
+        print("converting "+i)
+        tmp = tractmeasures.loc[tractmeasures['structureID'] == i]
+        tmp = tmp.drop(columns={'subjectID','structureID','nodeID'})
+        tmp.to_csv('profiles/'+i+'_profiles.csv',index=False)
 
 def main():
-
-	# load config
-	with open('config.json','r') as config_f:
-		config = json.load(config_f)
-
-	# create outpath
-	outpath = './profiles'
-	if ~os.path.isdir(outpath):
-		os.mkdir(outpath)
-	
-	# convert tractmeasures to tractprofile datatype
-	convert_tm(config["tractmeasures"])
+    with open('config.json','r') as config_f:
+        config = json.load(config_f)
+    if ~os.path.isdir(outpath):
+        os.mkdir(outpath)
+    convert_tm(config["tractmeasures"])
 
 if __name__ == '__main__':
-	main()
+    main()
